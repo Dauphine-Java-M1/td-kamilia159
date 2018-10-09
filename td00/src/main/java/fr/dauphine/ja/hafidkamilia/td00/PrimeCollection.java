8b785ab12e1b5af5c97bcd4900d33bda7c7b09cd
hafika18 <hafika18@b026-02.crio.dauphine.fr>
2018-10-09 17:25:38 +0200

    increase random gene-rator upper bound

--- td00/src/main/java/fr/dauphine/ja/hafidkamilia/td00/PrimeCollection.java
+++ td00/src/main/java/fr/dauphine/ja/hafidkamilia/td00/PrimeCollection.java
@@ -1,13 +1,68 @@
 package fr.dauphine.ja.hafidkamilia.td00;
+import java.util.ArrayList;
+
+import java.util.Random;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class PrimeCollection
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+	
+public  ArrayList <Integer> numbers; 
+
+	
+	public PrimeCollection(){
+		this.numbers= new ArrayList<Integer> () ;
+	}
+	
+	
+	public  void initRandom(int n , int m) {
+		int nombre_aleatoire ;
+		for(int i=0;i<n;i++) {
+	      nombre_aleatoire=(int)(Math.random()*(m)+1);
+	     ((java.util.ArrayList<Integer>) this.numbers).add(nombre_aleatoire);
+		}
+	}
+		
+		
+		
+
+	
+private boolean isPrime(int p){
+    int z=0;
+ 
+    for(int i=1;i<=p;i++) {
+         if (p%i==0)  z++;
+                                }
+    if (z==2)
+        return true;
+        else  return false;
+   
+   
+   
+}   
+public void printPrimes() {
+   
+	for (Integer t:numbers) {
+        if (isPrime(t)==true)
+        System.out.println(t);
+   
+       
     }
+   
 }
+public static void main (String[] args) {
+    PrimeCollection a = new PrimeCollection();
+
+ a.initRandom(100, 2000);
+ a.printPrimes();
+  
+    System.out.println(a.isPrime(7));
+    a.printPrimes();
+}
+}
+
+	
+
