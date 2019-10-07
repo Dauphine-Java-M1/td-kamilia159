97410642af7c7cca71a5049ca2804f2ce8c6309c
hafika18 <hafika18@b048-18.crio.dauphine.fr>
2019-10-07 14:47:04 +0200

    second commit

--- td00/src/main/java/fr/dauphine/ja/hafidkamilia/td00/PrimeCollection.java
+++ td00/src/main/java/fr/dauphine/ja/hafidkamilia/td00/PrimeCollection.java
@@ -1,4 +1,5 @@
 package fr.dauphine.ja.hafidkamilia.td00;
+
 import java.util.ArrayList;
 
 import java.util.Random;
@@ -7,17 +8,19 @@ import java.util.Random;
  * Hello world!
  *
  */
-public class PrimeCollection
-{
-	
-public  ArrayList <Integer> numbers; 
+public class PrimeCollection {
+
+	public ArrayList<Integer> numbers;
 
 	
-	public PrimeCollection(){
-		this.numbers= new ArrayList<Integer> () ;
+	
+	public PrimeCollection() {
+		this.numbers = new ArrayList<Integer>();
 	}
 	
 	
+	
+
 	public  void initRandom(int n , int m) {
 		int nombre_aleatoire ;
 		for(int i=0;i<n;i++) {
@@ -26,43 +29,55 @@ public  ArrayList <Integer> numbers;
 		}
 	}
 		
-		
-		
 
 	
-private boolean isPrime(int p){
-    int z=0;
+	
+	private boolean isPrime(int p) {
+		int z = 0;
+
+		for (int i = 1; i <= p; i++) {
+			if (p % i == 0)
+				z++;
+		}
+		if (z == 2)
+			return true;
+		else
+			return false;
+
+	}
+	
+	
+	
+	
+	
+
+	public void printPrimes() {
+		
+
+			for (Integer t:numbers) {
+			    if (isPrime(t)==true)
+			    System.out.println(t);
+
+			   
+			}
+		}
+
+	
+	
+
+	public static void main (String[] args) {
+	    PrimeCollection a = new PrimeCollection();
+
+	 a.initRandom(100, 3000);
+	 a.printPrimes();
+	  
+	    System.out.println(a.isPrime(7));
+	    a.printPrimes();
+	}
+
+	}
+
+
  
-    for(int i=1;i<=p;i++) {
-         if (p%i==0)  z++;
-                                }
-    if (z==2)
-        return true;
-        else  return false;
-   
-   
-   
-}   
-public void printPrimes() {
-   
-	for (Integer t:numbers) {
-        if (isPrime(t)==true)
-        System.out.println(t);
-   
-       
-    }
-   
-}
-public static void main (String[] args) {
-    PrimeCollection a = new PrimeCollection();
 
- a.initRandom(100, 2000);
- a.printPrimes();
-  
-    System.out.println(a.isPrime(7));
-    a.printPrimes();
-}
-}
-
-	
 
